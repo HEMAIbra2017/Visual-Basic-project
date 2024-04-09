e0407249abe00708d1ea78d2b13333b8afd16b96
HEMAIbra2017 <33623615+HEMAIbra2017@users.noreply.github.com>
2024-04-09 11:37:11 +0200

    Add files via upload

---
+++ RGB color/WindowsApplication111/WindowsApplication111/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Not IsNumeric(TextBox1.Text) Or Not IsNumeric(TextBox2.Text) Or Not IsNumeric(TextBox3.Text) Then
+            MsgBox("Enter val of color")
+        ElseIf TextBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Then
+            MsgBox("Enter val of color")
+
+        ElseIf TextBox1.Text > 255 Or TextBox2.Text > 255 Or TextBox3.Text > 255 Then
+
+            MsgBox("Enter value of color between 0 and 255")
+        ElseIf TextBox1.Text < 0 Or TextBox2.Text < 0 Or TextBox3.Text < 0 Then
+            MsgBox("enter val of color more than or equal 0")
+        Else : PictureBox1.BackColor = Color.FromArgb(Me.TextBox1.Text, Me.TextBox2.Text, Me.TextBox3.Text)
+
+
+
+        End If
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+End Class
